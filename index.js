355315e0d2f53be77c3fff77c09e7b3c8aef5ca0
Utkarsh Kumar <utkarsh9799@gmail.com>
2020-05-22 17:29:32 +0530

    Added OAuth verification & Google strategy

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
 
 // Initialising express app
 const app = express();
 
-// Handling routes
-app.get('/', (req, res) => {
-	res.send('Hello!');
-});
+passport.use(
+	new GoogleStrategy(
+		{
+			clientID: keys.googleClientID,
+			clientSecret: keys.googleClientSecret,
+			callbackURL: '/auth/google/callback',
+		},
+		(accessToken) => {
+			console.log(accessToken);
+		}
+	)
+);
+
+app.get(
+	'/auth/google',
+	passport.authenticate('google', {
+		scope: ['profile', 'email'],
+	})
+);
 
 // Listening on dynamic env port or 5000
 const PORT = process.env.PORT || 5000;
