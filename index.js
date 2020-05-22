f4f915fe4a191e614079221c096c8d78308043b1
Utkarsh Kumar <utkarsh9799@gmail.com>
2020-05-22 21:43:36 +0530

    Estabilished database connection

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
+
+// For including the authentication strategy
 require('./services/passport');
+
+// Estabilishing MongoDB connection
+mongoose.connect(keys.mongoURI, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+});
 
 // Initialising express app
 const app = express();
 
+// For handling authentication routes
 require('./routes/authRoutes')(app);
 
 // Listening on dynamic env port or 5000
