7ac649b40ac10cd1ad4a4258edc699baef522cdc
Utkarsh Kumar <utkarsh9799@gmail.com>
2020-05-25 21:40:53 +0530

    Production environment and keys setup

---
+++ config/keys.js
@@ -0,0 +1,8 @@
+// Deciding the set of keys to use
+if (process.env.NODE_ENV === 'production') {
+	//Production keys
+	module.exports = require('./prod');
+} else {
+	//Development keys
+	module.exports = require('./dev');
+}
