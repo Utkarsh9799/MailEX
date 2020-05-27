4ba345e7f64dc853c9394571760cefb2e2e28fb9
Utkarsh Kumar <utkarsh9799@gmail.com>
2020-05-27 21:33:27 +0530

    Added proxy middleware in client

---
+++ client/src/setupProxy.js
@@ -0,0 +1,10 @@
+// Proxy middleware
+const { createProxyMiddleware } = require('http-proxy-middleware');
+module.exports = function (app) {
+	app.use(
+		['/api', '/auth/google'],
+		createProxyMiddleware({
+			target: 'http://localhost:5000',
+		})
+	);
+};
