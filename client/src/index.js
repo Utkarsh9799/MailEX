2475c546320bdc3d0a2e7951fedfb25cb426a377
Utkarsh Kumar <utkarsh9799@gmail.com>
2020-05-29 22:38:05 +0530

    Redux setup

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
 
 import App from './components/App';
 
-ReactDOM.render(<App />, document.querySelector('#root'));
+// Creating a Redux store
+const store = createStore(() => [], {}, applyMiddleware());
+
+ReactDOM.render(
+	// Provider tag, a react component that can read changes from redux store anytime the state changes
+	<Provider store={store}>
+		<App />
+	</Provider>,
+	document.querySelector('#root')
+);
