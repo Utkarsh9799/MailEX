6c73869f92ec28da2447eea753f9f0d87e28d7ac
Utkarsh Kumar <utkarsh9799@gmail.com>
2020-06-03 17:35:54 +0530

    Action creator for handling token from Stripe

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -2,10 +2,18 @@
 import axios from 'axios';
 import { FETCH_USER } from './types';
 
-// Action creator
+// Action creator for fetching user info to know wheather user is logged in or not
 export const fetchUser = () => {
 	return async (dispatch) => {
 		const res = await axios.get('/api/current_user');
+		dispatch({ type: 'FETCH_USER', payload: res.data });
+	};
+};
+
+// Action creator to take the stripe token and send to backend API for biiling the user
+export const handleToke = (token) => {
+	return async (dispatch) => {
+		const res = await axios.post('/api/stripe', token);
 		dispatch({ type: 'FETCH_USER', payload: res.data });
 	};
 };
