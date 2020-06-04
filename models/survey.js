0939be59e6bf524f5d5c18fb469e8107b41a694b
Utkarsh Kumar <utkarsh9799@gmail.com>
2020-06-04 23:00:21 +0530

    Linked survey schema with user model collection

--- models/survey.js
+++ models/survey.js
@@ -9,6 +9,10 @@ const surveySchema = new Schema({
 	recipients: [recipientSchema],
 	yes: { type: Number, default: 0 },
 	no: { type: Number, default: 0 },
+	// Relationship field setting up relationship with user model
+	_user: { type: Schema.Types.ObjectId, ref: 'user' },
+	dateSent: Date,
+	lastResponded: Date,
 });
 
 mongoose.model('surveys', surveySchema);
