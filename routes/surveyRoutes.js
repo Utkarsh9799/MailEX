25d15727624db10d3897dca99cfa1dffa6815cd2
Utkarsh Kumar <utkarsh9799@gmail.com>
2020-06-07 17:40:36 +0530

    Deduct credits on sending emails

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -7,7 +7,7 @@ const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
 const survey = mongoose.model('surveys');
 
 module.exports = (app) => {
-	app.post('/api/surveys', requireLogin, requireCredits, (req, res) => {
+	app.post('/api/surveys', requireLogin, requireCredits, async (req, res) => {
 		const { title, subject, body, recipients } = req.body;
 
 		const newSurvey = new survey({
@@ -23,6 +23,11 @@ module.exports = (app) => {
 		});
 
 		const mailer = new Mailer(newSurvey, surveyTemplate(survey));
-		mailer.send();
+		await mailer.send();
+		await survey.save();
+		req.user.credits -= 1;
+		const user = await req.user.save();
+
+		res.send(user);
 	});
 };
