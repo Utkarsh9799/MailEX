7a7545da49453cb857c16302aae3077080251409
Utkarsh Kumar <utkarsh9799@gmail.com>
2020-06-17 18:26:04 +0530

    SurveyForm fields rendered using Field

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -1,9 +1,48 @@
 import React, { Component } from 'react';
+// Importing redux form helper to communicate with redux store and field component for rendering html tags
+import { reduxForm, Field } from 'redux-form';
+import SurveyField from './SurveyField';
+
+const Fields = [
+	{ label: 'Survey Title', name: 'title' },
+	{ label: 'Subject Line', name: 'subject' },
+	{ label: 'Email Body', name: 'body' },
+	{ label: 'Recipients List', name: 'emails' },
+];
 
 class SurveyForm extends Component {
+	renderFields() {
+		return Fields.map(({ name, label }) => {
+			return (
+				<Field
+					key={name}
+					type="text"
+					component={SurveyField}
+					name={name}
+					label={label}
+				/>
+			);
+		});
+	}
+
 	render() {
-		return <div>SurveyForm</div>;
+		return (
+			<div>
+				<form
+					onSubmit={this.props.handleSubmit((values) =>
+						console.log(values)
+					)}
+				>
+					{this.renderFields()}
+
+					<button type="submit">Submit</button>
+				</form>
+			</div>
+		);
 	}
 }
 
-export default SurveyForm;
+// Adding props to the class component
+export default reduxForm({
+	form: 'surveyForm',
+})(SurveyForm);
