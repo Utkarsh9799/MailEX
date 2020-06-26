ba3cd1c5d8b3caafeaa7c44138ad26ed69aa1905
Utkarsh Kumar <utkarsh9799@gmail.com>
2020-06-26 23:22:39 +0530

    Get req to /api/surveys endpoint to fetch surveys

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -10,6 +10,14 @@ const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
 const survey = mongoose.model('surveys');
 
 module.exports = (app) => {
+	app.get('/api/surveys', requireLogin, async (req, res) => {
+		const surveys = await survey.find({ _user: req.user.id }).select({
+			recipients: false,
+		});
+
+		res.send(surveys);
+	});
+
 	app.get('/api/surveys/:surveyId/:choice', (req, res) => {
 		res.send('Thanks for your feedback!');
 	});
